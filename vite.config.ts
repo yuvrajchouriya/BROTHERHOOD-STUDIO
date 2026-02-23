300ae225bb70043926fcf0d433f7110f95a34e03
yuvrajchouriya <yuvrajchouriya96@gmail.com>
2026-02-23 17:27:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",   // 🔥 IMPORTANT CHANGE
+  base: "./",
   build: {
     outDir: "dist",
   },
